89ffd5c515c2943a6343250d74efd1940a728eab
tusharagrahari <agrahari.tushar84@gmail.com>
2024-11-26 15:57:48 +0530

    Log incoming stream using fn handle_connection

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,18 @@ fn main () {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection Established");
+        handle_connection(stream);
     }
+}
+
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+    println!("Request: {http_request:#?}");
 }
