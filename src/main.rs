5d66be63973477db17ab156def299bd32711ce52
tusharagrahari <agrahari.tushar84@gmail.com>
2024-11-26 17:02:18 +0530

    Return an html response unconditionally

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use::std::{
+    fs,
     net::{TcpListener, TcpStream},
     io::{prelude::*, BufReader},
 };
@@ -23,6 +24,12 @@ fn handle_connection(mut stream: TcpStream){
 
     //println!("Request: {http_request:#?}");
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
     stream.write_all(response.as_bytes()).unwrap();
 }
