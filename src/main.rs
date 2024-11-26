ac925e6d9382931c0fd5bdf076ad636554638aff
tusharagrahari <agrahari.tushar84@gmail.com>
2024-11-26 14:27:26 +0530

    Establish Connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use::std::{
+    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader},
+};
+
+fn main () {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection Established");
+    }
 }
