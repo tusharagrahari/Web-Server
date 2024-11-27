7ac6d2502b968e740c926ac8dd58d4bd05ac9b62
tusharagrahari <agrahari.tushar84@gmail.com>
2024-11-27 12:24:20 +0530

    Implement delay function of 10sec if /sleep is called

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use::std::{
     fs,
     net::{TcpListener, TcpStream},
     io::{prelude::*, BufReader},
+    thread,
+    time::Duration
 };
 
 fn main () {
@@ -42,10 +44,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     //Refactored the above code to reduce code repetition.
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(10));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
